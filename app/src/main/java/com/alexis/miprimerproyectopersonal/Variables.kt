75d87f4fe58d14169a79383228f23cf679b6527e
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-12-16 19:19:21 -0600

    Esto es una prueba

---
+++ app/src/main/java/com/alexis/miprimerproyectopersonal/Variables.kt
@@ -0,0 +1,15 @@
+package com.alexis.miprimerproyectopersonal
+//HIIIIIIIII
+
+ fun main(){
+     print("HolaMundo")
+     val variable = "---Hola Jejejej";
+     print(variable)
+     //Varible INT
+     val edad: Int = 30
+
+     //Long
+     val edadgrande: Long = 30
+     val EdadDecimal: Float = 30.6f
+     var EdadDoble: Double = 4.3
+ }
