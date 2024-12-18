5a97feeb7d70592a7af0b5383ee083ba20167c96
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2024-12-18 13:42:23 -0600

    Variables

--- app/src/main/java/com/alexis/miprimerproyectopersonal/Variables.kt
+++ app/src/main/java/com/alexis/miprimerproyectopersonal/Variables.kt
@@ -2,14 +2,25 @@ package com.alexis.miprimerproyectopersonal
 //HIIIIIIIII
 
  fun main(){
-     print("HolaMundo")
+     println("HolaMundo")
      val variable = "---Hola Jejejej";
-     print(variable)
+     println(variable)
      //Varible INT
      val edad: Int = 30
+     var edad3: Int = 30
+     println(edad3)
+     edad3 = 3
+     println(edad3)
 
      //Long
      val edadgrande: Long = 30
      val EdadDecimal: Float = 30.6f
      var EdadDoble: Double = 4.7
+
+     //
+     val variable2:Char = 'd'
+
+     val vari:String = "holaaaaa"
+     val vaei2:Boolean = false
+     val vaei3:Boolean = false
  }
